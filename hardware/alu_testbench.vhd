80e9a5cc73175c8bd662ef52232e126b6cc99db9
phil <phil.pieper@studium.uni-hamburg.de>
2024-11-28 16:53:25 +0100

    fix: alu use numeric lib core: add alu testbench

--- hardware/alu_testbench.vhd
+++ hardware/alu_testbench.vhd
@@ -1,6 +1,7 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-use IEEE.std_logic_unsigned.all;
+use ieee.NUMERIC_STD.all;
+
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -15,30 +16,29 @@ ARCHITECTURE behavior OF tb_ALU IS
     COMPONENT ALU
     PORT(
         -- inputs:
-        A        : in std_logic_vector(15 downto 0);
-        B        : in std_logic_vector(15 downto 0);
-        I           : in std_logic_vector(4 downto 0);
-        out_alu     : out std_logic_vector(15 downto 0);
-        carryout_alu: out std_logic;
+        A        : in signed(15 downto 0);
+        B        : in signed(15 downto 0);
+        I           : in signed(4 downto 0);
+        out_alu     : out signed(15 downto 0);
+        carryout_alu: out std_logic
         );
     END COMPONENT;
 
-   signal A : std_logic_vector(15 downto 0) := (others => '0');
-   signal B : std_logic_vector(15 downto 0) := (others => '0');
-   signal I : std_logic_vector(4 downto 0) := (others => '0');
+   signal A : signed(15 downto 0) := (others => '0');
+   signal B : signed(15 downto 0) := (others => '0');
+   signal I : signed(4 downto 0) := (others => '0');
 
   --Outputs
-   signal out_alu : std_logic_vector(15 downto 0);
+   signal out_alu : signed(15 downto 0);
    signal carryout_alu : std_logic;
- 
- signal i:integer;
+
 BEGIN
     -- Instantiate the Unit Under Test (UUT)
     uut: ALU PORT MAP (
         A => A,
         B => B,
         I => I,
-        out_alu => out_alu
+        out_alu => out_alu,
         carryout_alu => carryout_alu
     );
 
@@ -47,8 +47,8 @@ BEGIN
         -- Test 1: Addition (A + B)
         A <= x"0005";
         B <= x"0003";
-        I <= "00000"
-        wait for 10 ns;
+        I <= "00000";
+        finish;
     end process;
 
 END;
