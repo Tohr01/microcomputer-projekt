821afe5da5fd519972dedcaa09ddad122fecfd01
phil <phil.pieper@studium.uni-hamburg.de>
2024-11-28 17:22:09 +0100

    feat: running script and stop pf testbench process

--- hardware/alu_testbench.vhd
+++ hardware/alu_testbench.vhd
@@ -1,8 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 use ieee.NUMERIC_STD.all;
-
-
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 -- USE ieee.numeric_std.ALL;
@@ -31,7 +29,7 @@ ARCHITECTURE behavior OF tb_ALU IS
   --Outputs
    signal out_alu : signed(15 downto 0);
    signal carryout_alu : std_logic;
-
+   
 BEGIN
     -- Instantiate the Unit Under Test (UUT)
     uut: ALU PORT MAP (
@@ -45,10 +43,12 @@ BEGIN
     stim_proc: process
     begin  
         -- Test 1: Addition (A + B)
-        A <= x"0005";
-        B <= x"0003";
-        I <= "00000";
-        finish;
+        for iterator in 0 to 15 loop
+            A <= x"0005";
+            B <= x"0003";
+            I <= "00000";
+            wait for 10 ms;
+        end loop;
     end process;
 
 END;
