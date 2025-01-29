a00438dfd82bdaaa39f8d9eb60d78692136f802f
Tohr01 <carl@kratzundraabe.de>
2025-01-29 19:48:13 +0100

    refactor: remove print

--- software/asm_config/registers.py
+++ software/asm_config/registers.py
@@ -26,18 +26,3 @@ class REGISTER_BANK:
             return reg_bin
         except KeyError:
             raise KeyError(f'{register_name} is not a valid register')
-
-
-"""
-reg_dict = {}
-for i in range(len(REGISTER_BANK.REGISTERS) - 1, -1, -1):
-    assert i < 32
-    register_name = REGISTER_BANK.REGISTERS[i]
-    num = 32 - len(REGISTER_BANK.REGISTERS) + i
-    reg_dict[register_name] = num
-
-for k, v in reversed(reg_dict.items()):
-    print(f'"{k}": {v}')
-"""
-
-print(format(2**16 - 1, 'b').zfill(5))
